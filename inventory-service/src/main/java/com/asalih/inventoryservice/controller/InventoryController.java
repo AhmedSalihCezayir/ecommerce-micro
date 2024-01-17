3d0066a0e05a69c973ac55a4a9fb7257ba30d6bb
Ahmed Salih Cezayir <asalihcezayir@gmail.com>
2024-01-17 21:58:19 +0300

    A basic inventory service is created

---
+++ inventory-service/src/main/java/com/asalih/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,19 @@
+package com.asalih.inventoryservice.controller;
+
+import com.asalih.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+    private final InventoryService inventoryService;
+
+    @GetMapping("/{skuCode}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable String skuCode) {
+        return inventoryService.isInStock(skuCode);
+    }
+}
